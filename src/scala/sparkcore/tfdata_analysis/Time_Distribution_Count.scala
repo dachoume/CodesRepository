0b4cad8fc4e656b6a373cdd7acedc6f6d2c68aff
da-cou <18633598458@163.com>
2018-03-22 17:05:05 +0800

    Add files via upload

---
+++ src/scala/sparkcore/tfdata_analysis/Time_Distribution_Count.scala
@@ -0,0 +1,10 @@
+package data_analysis_tf
+
+/**
+  * Created by Administrator on 2017/8/16.
+  */
+case class Time_Distribution_Count(year:String,
+                                   Month:String,
+                                   day:String,
+                                   hour:String,
+                                   count:Integer)
