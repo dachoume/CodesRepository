3e79f4e22f15c73c5eacd5a276de9e6a8609cd3f
da-cou <18633598458@163.com>
2018-03-22 17:03:23 +0800

    Add files via upload

---
+++ src/scala/sparkcore/tfdata_analysis/Time_Distribution_Count_D.scala
@@ -0,0 +1,9 @@
+package data_analysis_tf
+
+/**
+  * Created by Administrator on 2017/8/28.
+  */
+case class Time_Distribution_Count_D(year: String,
+                                     Month: String,
+                                     day: String,
+                                     count: Integer)
---
+++ src/scala/sparkcore/tfdata_analysis/Time_Distribution_Count_M.scala
@@ -0,0 +1,8 @@
+package data_analysis_tf
+
+/**
+  * Created by Administrator on 2017/8/28.
+  */
+case class Time_Distribution_Count_M(year: String,
+                                     Month: String,
+                                     count: Integer)
