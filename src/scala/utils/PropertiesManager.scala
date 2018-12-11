4c21bb1a25b0aa3dbb76c27a24b5c58ab949f506
da-cou <18633598458@163.com>
2018-12-11 16:00:18 +0800

    Create PropertiesManager.scala

---
+++ src/scala/utils/PropertiesManager.scala
@@ -0,0 +1,69 @@
+package utils
+
+import java.util.Properties
+import java.io.{BufferedInputStream, FileInputStream, InputStream}
+
+class PropertiesManager extends Serializable {
+    private val properties: Properties = new Properties()
+
+    def getProperties(filename: String, options: String): String = {
+        var in = None: Option[InputStream] //初始化一个为None的流，使用Some
+        try {
+            in = Some(this.getClass().getClassLoader.getResourceAsStream(filename)) //文件要放到resources文件夹下
+            properties.load(in.get)
+            properties.getProperty(options) //读取键为options的数据的值
+        } catch {
+            case e: Exception => "fileerror" + "   " + e.getMessage
+        } finally {
+            if (in.isDefined) {
+                if (in.get != null) {
+                    try {
+                        in.get.close()
+                    } catch {
+                        case e: Exception => "fileerror" + "   " + e.getMessage
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    def getPropertiesByPath(filePath: String, options: String): String = {
+        var in = None: Option[InputStream] //初始化一个为None的流，使用Some
+        try {
+            in = Some(new BufferedInputStream(new FileInputStream(filePath))) //文件要放到resources文件夹下
+            properties.load(in.get)
+            properties.getProperty(options) //读取键为options的数据的值
+        } catch {
+            case e: Exception => "fileerror" + "   " + e.getMessage
+        } finally {
+            if (in.isDefined) {
+                if (in.get != null) {
+                    try {
+                        in.get.close()
+                    } catch {
+                        case e: Exception => "fileerror" + "   " + e.getMessage
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
+object PropertiesManager {
+    var propertiesManager: PropertiesManager = _
+
+    def getPropertiesManager: PropertiesManager = {
+        synchronized {
+            if (propertiesManager == null) {
+                propertiesManager = new PropertiesManager
+            }
+        }
+        propertiesManager
+    }
+
+
+}
