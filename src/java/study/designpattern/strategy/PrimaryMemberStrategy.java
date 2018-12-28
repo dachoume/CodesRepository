399185e69658dd6e6f5510f9a3f8f7fed2f9757e
da-cou <18633598458@163.com>
2018-12-28 15:45:45 +0800

    Create PrimaryMemberStrategy.java

---
+++ src/java/study/designpattern/strategy/PrimaryMemberStrategy.java
@@ -0,0 +1,16 @@
+package study.designpattern.strategy;
+
+/**
+ * describe:
+ *
+ * @author dadou
+ * @date 2018/12/28
+ */
+public class PrimaryMemberStrategy implements MemberStrategy {
+    @Override
+    public double calcPrice(double booksPrice) {
+
+        System.out.println("对于初级会员的没有折扣");
+        return booksPrice;
+    }
+}
